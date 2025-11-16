833b54408a4fcad69308538da45f8d71b57bd3a6
tmatias <tmatias0406@gmail.com>
2025-11-16 15:52:05 -0600

    Fix hosts for DigitalOcean

--- carnes_del_rancho/settings.py
+++ carnes_del_rancho/settings.py
@@ -131,19 +131,20 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEBUG = False
 
 # === Media (Local vs DigitalOcean Spaces) ================================
-MEDIA_URL = "/media/"
+MEDIA_URL = "https://carnes-del-rancho-media.nyc3.digitaloceanspaces.com/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 if not DEBUG:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
     AWS_ACCESS_KEY_ID = os.getenv("SPACES_KEY")
     AWS_SECRET_ACCESS_KEY = os.getenv("SPACES_SECRET")
     AWS_STORAGE_BUCKET_NAME = os.getenv("SPACES_BUCKET_NAME")
     AWS_S3_ENDPOINT_URL = os.getenv("SPACES_ENDPOINT")
 
+    AWS_S3_REGION_NAME = "nyc3"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_DEFAULT_ACL = "public-read"
-
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 else:
     DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 
