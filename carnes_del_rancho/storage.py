da9bd7fdd753d24106b121b2394650904313496e
tmatias <tmatias0406@gmail.com>
2025-11-21 13:57:11 -0600

    fix: updated image URLs

--- carnes_del_rancho/storage.py
+++ carnes_del_rancho/storage.py
@@ -1,14 +1,7 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class OverwriteStorage(S3Boto3Storage):
-    """
-    Storage que sobrescribe archivos existentes en DigitalOcean Spaces.
-    No permite que Django genere nombres con hash (_ASD123.png).
-    """
+class StaticNameStorage(S3Boto3Storage):
     file_overwrite = True
-
     def get_available_name(self, name, max_length=None):
-        # Si el archivo existe en Spaces, se borra primero
-        if self.exists(name):
-            self.delete(name)
+        # NO permite agregar sufijos, usa el nombre tal cual
         return name
