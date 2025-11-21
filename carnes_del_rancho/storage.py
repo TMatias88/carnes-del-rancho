9752d4f82b4cc845e3f3cc82aa448db57ab119a9
tmatias <tmatias0406@gmail.com>
2025-11-21 13:37:24 -0600

    fix: updated image URLs

--- carnes_del_rancho/storage.py
+++ carnes_del_rancho/storage.py
@@ -1,4 +1,13 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticNameStorage(S3Boto3Storage):
-    file_overwrite = True   # ← NO PERMITE CAMBIAR NOMBRES
+class OverwriteStorage(S3Boto3Storage):
+    file_overwrite = True
+
+    def get_available_name(self, name, max_length=None):
+        """
+        Evita que Django renombre el archivo si existe.
+        Si existe, lo borra primero.
+        """
+        if self.exists(name):
+            self.delete(name)
+        return name
